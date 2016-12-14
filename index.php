f9dc00a94dca6548ec5b64254d656f108d0c6eaf
franckisys <franckisys@gmail.com>
2016-12-14 01:02:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@ INSER CLI.PY
 
 <?php 
 
-$command = escapeshellcmd('/var/www/html/snomcli/cgi-bin/form.py');
+$command = escapeshellcmd('/var/www/html/snomcli/cgi-bin/httplib.py');
 $output = shell_exec($command);
 echo $output;
 
